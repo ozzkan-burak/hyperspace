8bc58a35c86977ade6ce5b2f3acb1ed02a458948
ozzkan-burak <brk.ozzkan@gmail.com>
2024-11-12 23:23:41 +0300

    episode2

--- hyperspace/core/views.py
+++ hyperspace/core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Wecome to Hyperspace, this message with Core")
+  return render(request, "index.html")
 
